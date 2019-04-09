88cbb9c79e688148b184a94e2584dadbc2d84bd7
ValeriaGA <valeriaga1000@gmail.com>
2019-04-08 19:13:53 -0600

    Update buildCity.cs

--- Spiderman_Game_IA2/Assets/Scripts/buildCity.cs
+++ Spiderman_Game_IA2/Assets/Scripts/buildCity.cs
@@ -79,14 +79,72 @@ namespace IA_Proyecto_1
             this.m = m;
             
         }
+
+
+        public void build_city()
+        {
+            map = new Grid(n, m);
+            //float seed = Random.Range(0, 100);
+
+            for (int h = 0; h < map.height; h++)
+            {
+                for (int w = 0; w < map.width; w++)
+                {
+                    //int result = (int)(Mathf.PerlinNoise(w / 10.0f + seed, h / 10.0f + seed) * 10);
+                    Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint); 
+                    int n = Random.Range(0, buildings.Length);
+                    Instantiate(buildings[n], pos, Quaternion.identity);
+                    if (n == 5)
+                    {
+                        map.obstructed.Add(new Point(w, h));
+                    }
+                }
+            }
+            Point origin = new Point(Random.Range(0, map.height - 1), Random.Range(0, map.width - 1));
+            while (map.obstructed.Contains(origin))
+            {
+                origin = new Point(Random.Range(0, map.height - 1), Random.Range(0, map.width - 1));
+            }
+                
+            Point goal = new Point(Random.Range(0, map.height - 1), Random.Range(0, map.width - 1));
+            while (map.obstructed.Contains(goal))
+            {
+                goal = new Point(Random.Range(0, map.height - 1), Random.Range(0, map.width - 1));
+            }
+
+            Vector3 posWoman = new Vector3(goal.X+1 * buildingFootprint, 4, goal.Y * buildingFootprint);
+            Instantiate(people[0], posWoman, Quaternion.identity);
+
+            Vector3 posSpiderman = new Vector3(origin.X * buildingFootprint, 5, origin.Y * buildingFootprint);
+            Instantiate(people[1],posSpiderman , Quaternion.identity);
+
+
+            AStar astar = new AStar(map, origin, goal, diagonal);
+            if (astar.cameFrom.ContainsKey(goal))
+            {
+                Debug.LogFormat("Found path from ({0},{1}) to ({2},{3}) = \n", origin.X.ToString(), origin.Y.ToString(), goal.X.ToString(), goal.Y.ToString());
+                Point current = goal;
+                while (current != origin)
+                {
+                    Debug.LogFormat("({0},{1}) <- ", current.X.ToString(), current.Y.ToString());
+                    current = astar.cameFrom[current];
+                }
+            }
+            else
+            {
+                Debug.LogFormat("No path found from ({0},{1}) to ({2},{3}) = \n", origin.X.ToString(), origin.Y.ToString(), goal.X.ToString(), goal.Y.ToString());
+            }
+        }
+
+
         public void moveSpiderman(Point newPosition)
         {
-            
+            people[1].transforme.Translate(newPosition.X * buildingFootprint, 4, newPosition.Y * buildingFootprint);   
             
         }
         public void moveWoman(Point newPosition)
         {
-            
+            people[0].transforme.Translate(newPosition.X * buildingFootprint, 4, newPosition.Y * buildingFootprint);
             
         }
 
